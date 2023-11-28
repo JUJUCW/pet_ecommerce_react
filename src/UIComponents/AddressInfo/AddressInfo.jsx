15b148c97d6d785e047fa31c6742d3aafe1c74f4
JUJUCW <juchung424@gmail.com>
2023-11-28 16:29:12 +0800

    FEAT/ add address section

--- src/UIComponents/AddressInfo/AddressInfo.jsx
+++ src/UIComponents/AddressInfo/AddressInfo.jsx
@@ -1,7 +1,108 @@
+import { useState } from 'react';
 import styles from './AddressInfo.module.scss';
+import Stepper from 'UIComponents/Stepper/Stepper';
 
 export default function AddressInfo() {
-  return <div className={styles.container}>
-      AddressInfo
-    </div>;
+    const [currentStep, setCurrentStep] = useState(1);
+    const [form, setForm] = useState({
+        salutation: '先生',
+        name: '',
+        phone: '',
+        email: '',
+        city: '',
+        address: '',
+        shippingFee: 0,
+        ccName: '',
+        ccNumber: '',
+        ccExpireDate: '',
+        ccCvc: '',
+    });
+    const handleInputChange = (fieldName, value) => {
+        setForm({ ...form, [fieldName]: value });
+    };
+    return (
+        <div className={styles.container}>
+            <Stepper />
+            <h3>寄送地址</h3>
+            <div className={styles.personalInfo}>
+                <div className={styles.nameContainer}>
+                    <div className={styles.salutation}>
+                        <label htmlFor="salutation">稱謂</label>
+                        <select
+                            name="salutation"
+                            id="salutation"
+                            value={form.salutation}
+                            onChange={(e) => handleInputChange('salutation', e.target.value)}
+                        >
+                            <option value="先生">先生</option>
+                            <option value="小姐">小姐</option>
+                        </select>
+                    </div>
+                    <div className={styles.name}>
+                        <label htmlFor="name">姓名</label>
+                        <input
+                            type="text"
+                            name="name"
+                            id="name"
+                            placeholder="請輸入姓名"
+                            value={form.name}
+                            onChange={(e) => setForm({ ...form, name: e.target.value })}
+                        />
+                    </div>
+                </div>
+
+                <div className={styles.contact}>
+                    <div className={styles.phone}>
+                        <label htmlFor="phone">電話</label>
+                        <input
+                            type="text"
+                            name="phone"
+                            id="phone"
+                            placeholder="請輸入電話"
+                            value={form.phone}
+                            onChange={(e) => handleInputChange('name', e.target.value)}
+                        />
+                    </div>
+                    <div className={styles.email}>
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="text"
+                            name="email"
+                            id="email"
+                            placeholder="請輸入Email"
+                            value={form.email}
+                            onChange={(e) => setForm({ ...form, email: e.target.value })}
+                        />
+                    </div>
+                </div>
+                <div className={styles.city}>
+                    <div className={styles.cityContainer}>
+                        <label htmlFor="city">縣市</label>
+                        <select
+                            name="city"
+                            id="city"
+                            value={form.city}
+                            onChange={(e) => setForm({ ...form, city: e.target.value })}
+                        >
+                            <option value="台北市">台北市</option>
+                            <option value="新北市">新北市</option>
+                            <option value="台中市">台中市</option>
+                            <option value="嘉義縣">嘉義縣</option>
+                        </select>
+                    </div>
+                    <div className={styles.address}>
+                        <label htmlFor="email">地址</label>
+                        <input
+                            type="text"
+                            name="address"
+                            id="address"
+                            placeholder="請輸入地址"
+                            value={form.address}
+                            onChange={(e) => setForm({ ...form, address: e.target.value })}
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -1,13 +1,13 @@
 import Footer from 'components/Footer/Footer';
 import styles from './CartPage.module.scss';
 import NavBar from 'UIComponents/NavBar/NavBar';
-import Cart from 'UIComponents/Cart/Cart';
+import CartContainer from 'components/CartContainer/CartContainer';
 
 export default function CartPage() {
     return (
         <div className={styles.container}>
             <NavBar />
-            <Cart />
+            <CartContainer />
             <Footer />
         </div>
     );
