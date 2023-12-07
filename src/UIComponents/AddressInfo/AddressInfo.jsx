19fbc3a6bf9f5f8dab68297350ee342657188643
JUJUCW <juchung424@gmail.com>
2023-12-07 16:03:05 +0800

    FEAT/ add toggle for address Box

--- src/UIComponents/AddressInfo/AddressInfo.jsx
+++ src/UIComponents/AddressInfo/AddressInfo.jsx
@@ -1,110 +1,148 @@
 import { useState } from 'react';
 import styles from './AddressInfo.module.scss';
-import Stepper from 'UIComponents/Stepper/Stepper';
-import NextButton from 'UIComponents/NextButton/NextButton';
+// import Button from 'UIComponents/Button/Button';
+// import Stepper from 'UIComponents/Stepper/Stepper';
+// import NextButton from 'UIComponents/NextButton/NextButton';
 
 export default function AddressInfo() {
-    const [currentStep, setCurrentStep] = useState(1);
+    // const [currentStep, setCurrentStep] = useState(1);
     const [form, setForm] = useState({
-        salutation: '先生',
+        salutation: 'Mr.',
         name: '',
         phone: '',
         email: '',
-        city: '',
+        city: 'Taipei City',
         address: '',
         shippingFee: 0,
-        ccName: '',
-        ccNumber: '',
-        ccExpireDate: '',
-        ccCvc: '',
     });
+    const [isAddressVisible, setIsAddressVisible] = useState(true);
+    
+    const toggleAddressVisible = () => {
+        setIsAddressVisible(!isAddressVisible);
+    };
     const handleInputChange = (fieldName, value) => {
         setForm({ ...form, [fieldName]: value });
+    };
+    const handleSave = () => {
+    
+        toggleAddressVisible();
     };
     return (
         <>
             <div className={styles.container}>
                 {/* <Stepper /> */}
-                <h3>Shipping Address</h3>
-                <div className={styles.personalInfo}>
-                    <div className={styles.nameContainer}>
-                        <div className={styles.salutation}>
-                            <label htmlFor="salutation">Salutation</label>
-                            <select
-                                name="salutation"
-                                id="salutation"
-                                value={form.salutation}
-                                onChange={(e) => handleInputChange('salutation', e.target.value)}
-                            >
-                                <option value="先生">Mr.</option>
-                                <option value="小姐">Ms.</option>
-                            </select>
+                <h3 onClick={toggleAddressVisible}>Shipping Address</h3>
+                {!isAddressVisible && (
+                    <div className={styles.addressBox}>
+                        <div>
+                            <div className={styles.addressContentName}>
+                                {/* {saveName}&nbsp; */}
+                                {form.salutation}&nbsp;
+                                {form.name}
+                            </div>
+                            <div className={styles.addressContent}>
+                                {form.city}&nbsp;
+                                {form.address}
+                                {/* {saveAddress} */}
+                            </div>
                         </div>
-                        <div className={styles.name}>
-                            <label htmlFor="name">Full Name</label>
-                            <input
-                                type="text"
-                                name="name"
-                                id="name"
-                                placeholder="Enter Name Here"
-                                value={form.name}
-                                onChange={(e) => setForm({ ...form, name: e.target.value })}
-                            />
-                        </div>
-                    </div>
 
-                    <div className={styles.contact}>
-                        <div className={styles.phone}>
-                            <label htmlFor="phone">Phone</label>
-                            <input
-                                type="text"
-                                name="phone"
-                                id="phone"
-                                placeholder="Enter Phone Here"
-                                value={form.phone}
-                                onChange={(e) => handleInputChange('name', e.target.value)}
-                            />
-                        </div>
-                        <div className={styles.email}>
-                            <label htmlFor="email">Email</label>
-                            <input
-                                type="text"
-                                name="email"
-                                id="email"
-                                placeholder="Enter Email Here"
-                                value={form.email}
-                                onChange={(e) => setForm({ ...form, email: e.target.value })}
-                            />
-                        </div>
+                        <button className={styles.changeButton} onClick={toggleAddressVisible}>
+                            change
+                        </button>
                     </div>
-                    <div className={styles.city}>
-                        <div className={styles.cityContainer}>
-                            <label htmlFor="city">City</label>
-                            <select
-                                name="city"
-                                id="city"
-                                value={form.city}
-                                onChange={(e) => setForm({ ...form, city: e.target.value })}
-                            >
-                                <option value="台北市">台北市</option>
-                                <option value="新北市">新北市</option>
-                                <option value="台中市">台中市</option>
-                                <option value="嘉義縣">嘉義縣</option>
-                            </select>
+                )}
+                {isAddressVisible && (
+                    <>
+                        <div className={styles.personalInfo}>
+                            <button className={styles.cross} onClick={toggleAddressVisible}>
+                                X
+                            </button>
+                            <div className={styles.nameContainer}>
+                                <div className={styles.salutation}>
+                                    <label htmlFor="salutation">Salutation</label>
+                                    <select
+                                        name="salutation"
+                                        id="salutation"
+                                        value={form.salutation}
+                                        onChange={(e) => handleInputChange('salutation', e.target.value)}
+                                    >
+                                        <option value="Mr.">Mr.</option>
+                                        <option value="Ms.">Ms.</option>
+                                    </select>
+                                </div>
+                                <div className={styles.name}>
+                                    <label htmlFor="name">Full Name</label>
+                                    <input
+                                        type="text"
+                                        name="name"
+                                        id="name"
+                                        placeholder="Enter Name Here"
+                                        value={form.name}
+                                        onChange={(e) => handleInputChange('name', e.target.value)}
+                                    />
+                                </div>
+                            </div>
+
+                            <div className={styles.contact}>
+                                <div className={styles.phone}>
+                                    <label htmlFor="phone">Phone</label>
+                                    <input
+                                        type="tel"
+                                        pattern="[0-9]{4}-[0-9]{6}"
+                                        name="phone"
+                                        id="phone"
+                                        placeholder="0912-345678"
+                                        value={form.phone}
+                                        onChange={(e) => handleInputChange('phone', e.target.value)}
+                                        required
+                                    />
+                                </div>
+                                <div className={styles.email}>
+                                    <label htmlFor="email">Email</label>
+                                    <input
+                                        type="email"
+                                        name="email"
+                                        id="email"
+                                        placeholder="Enter Email Here"
+                                        value={form.email}
+                                        onChange={(e) => handleInputChange('email', e.target.value)}
+                                    />
+                                </div>
+                            </div>
+                            <div className={styles.city}>
+                                <div className={styles.cityContainer}>
+                                    <label htmlFor="city">City</label>
+                                    <select
+                                        name="city"
+                                        id="city"
+                                        value={form.city}
+                                        onChange={(e) => handleInputChange('city', e.target.value)}
+                                    >
+                                        <option value="Taipei City">Taipei City</option>
+                                        <option value="New Taipei City">New Taipei City</option>
+                                        <option value="Taichung City">Taichung City</option>
+                                        <option value="Kaohsiung City">Kaohsiung City</option>
+                                    </select>
+                                </div>
+                                <div className={styles.address}>
+                                    <label htmlFor="email">Address</label>
+                                    <input
+                                        type="text"
+                                        name="address"
+                                        id="address"
+                                        placeholder="Enter Address Here"
+                                        value={form.address}
+                                        onChange={(e) => handleInputChange('address', e.target.value)}
+                                    />
+                                </div>
+                            </div>
                         </div>
-                        <div className={styles.address}>
-                            <label htmlFor="email">Address</label>
-                            <input
-                                type="text"
-                                name="address"
-                                id="address"
-                                placeholder="Enter Address Here"
-                                value={form.address}
-                                onChange={(e) => setForm({ ...form, address: e.target.value })}
-                            />
-                        </div>
-                    </div>
-                </div>
+                        <button className={styles.saveButton} onClick={handleSave}>
+                            SAVE
+                        </button>
+                    </>
+                )}
             </div>
             {/* <NextButton /> */}
         </>
